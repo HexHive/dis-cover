70304059c69d1719ee5fa24326a55a42ac41ce76
Louis Merlin <louis.merlin@epfl.ch>
2021-03-19 14:13:06 +0100

    Simplify inheritance code to a bare minimum

--- case-studies/simple_inheritance.cpp
+++ case-studies/simple_inheritance.cpp
@@ -1,34 +1,23 @@
-#include<iostream>
-using namespace std;
-
 class A {
- public:
-  int a;
-  virtual void f1() {
-    cout << "Af1()\n";
-  }
-  virtual void f2() {
-    cout << "Af2()\n";
-  }
+public:
+  virtual void f() {}
 };
 
 class B : public A {
-  public:
-    int b;
-  virtual void f1() {
-    cout << "Bf1\n";
-  }
+public:
+  virtual void f() {}
 };
 
 int main(int argc, char *argv[]) {
-  A *a;
+  A *a_or_b;
+
   if (argc == 0) {
-    a = new A();
+    a_or_b = new A();
   } else {
-    a = new B();
+    a_or_b = new B();
   }
-  a->f1();
-  a->f2();
-  a->f1();
-  delete a;
+
+  a_or_b->f();
+
+  delete a_or_b;
 }
