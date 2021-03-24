b4cfc6c54e58e314b8558c839058482860ed41d4
Louis Merlin <louis.merlin@epfl.ch>
2021-03-24 11:40:04 +0100

    Start table labeling

--- dis-cover/analysis.py
+++ dis-cover/analysis.py
@@ -1,6 +1,7 @@
 from elftools.elf.elffile import ELFFile
 from capstone import *
 from capstone.x86_const import *
+from itanium_demangler import parse as demangle
 
 
 class Entry:
@@ -34,9 +35,18 @@ class Table:
 
     def __init__(self, address):
         self.address = address
-        self.is_rtti = False
+        self.is_RTTI = False
         self.is_vtable = False
         self.entries = []
+        self.inherits_from = []
+        self.associated_RTTI = None
+
+    def get_name(self):
+        if self.is_RTTI:
+            return self.name
+        elif self.is_vtable and self.associated_RTTI:
+            return self.associated_RTTI.get_name()
+        return "Unknown Name"
 
 
 class Analysis:
@@ -61,6 +71,23 @@ class Analysis:
             if addr < end:
                 return name
         return "out of bounds"
+
+    def extract_name(self, addr):
+        section_name = self.get_section_name(int(addr, 16))
+        section = self.elffile.get_section_by_name(section_name)
+        section_data = section.data()
+        relative_address = int(addr, 16) - section["sh_addr"]
+        if relative_address > 0 and relative_address < len(section_data):
+            # name = '_ZTS'
+            name = ""
+            while (
+                relative_address < len(section_data)
+                and section_data[relative_address] != 0
+            ):
+                name += chr(section_data[relative_address])
+                relative_address += 1
+            # print(demangle(name))
+            return name
 
     def find_vfunc_calls(self):
 
@@ -116,8 +143,8 @@ class Analysis:
 
         # TODO
 
-        # Step 2 : Find all potential vtables by finding empty spaces that might
-        #          be the offset-to-top section.
+        # Step 2 : Find all potential tables by finding empty spaces that might
+        #          be the offset-to-top section, and create a Table for them.
 
         data_section = self.elffile.get_section_by_name(".data.rel.ro")
         data = data_section.data()
@@ -149,7 +176,25 @@ class Analysis:
         #          finding the pointers to RTTIs at the beginning of the vtables
         #          and in other related RTTIs.
 
-        # TODO
+        for table in self.tables:
+            for (i, entry) in enumerate(table.entries):
+                # If the first entry after the offset_to_top is a pointer to
+                # an RTTI, then the table is a vtable.
+                if i == 1 and entry.section == ".data.rel.ro":
+                    table.is_vtable = True
+                    # TODO find the table object associated with this address
+                    table.associated_RTTI = entry.value
+                # Else if the first entry after the offset_to_top is a pointer
+                # to __type_name, then the table is an RTTI.
+                elif i == 1 and entry.section == ".rodata":
+                    table.is_RTTI = True
+                    table.name = self.extract_name(entry.value)
+                    print(table.name)
+                # Else if the table is an RTTI and the entry is a pointer to
+                # another RTTI, then there is inheritance.
+                elif table.is_RTTI and entry.section == ".data.rel.ro":
+                    # TODO find the table object associated with this address
+                    table.inherits_from.append(entry.value)
 
 
 def analyse(elf_file_name):
