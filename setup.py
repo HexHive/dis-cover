a565a7a43f1b96cea84288492a2c07e419402b95
Louis Merlin <louis.merlin@epfl.ch>
2021-03-30 10:42:39 +0200

    Fix CLI method again

--- setup.py
+++ setup.py
@@ -100,7 +100,7 @@ setup(
     # If your package is a single module, use this instead of 'packages':
     py_modules=[NAME],
     entry_points={
-        "console_scripts": ["dis-cover=main"],
+        "console_scripts": ["dis-cover=dis-cover:main"],
     },
     install_requires=REQUIRED,
     extras_require=EXTRAS,
