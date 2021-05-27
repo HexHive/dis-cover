bf03267e1a502ccdb6de78da8958c4bc34ed514a
Louis Merlin <louis.merlin@epfl.ch>
2021-05-27 16:02:29 +0200

    Update package analysis (26/04/2021)

--- packages_analysis/source.py
+++ packages_analysis/source.py
@@ -4,16 +4,12 @@ import pickle
 import uuid
 from multiprocessing import Pool
 
+ADD_SOURCE_LIST = "echo \"deb-src http://http.us.debian.org/debian unstable main\" >> /etc/apt/sources.list"
 UPDATE_COMMAND = "apt-get update"
 GCC_RDEPENDENCIES_COMMAND = "apt-cache rdepends libgcc1"  # Debian
 # GCC_RDEPENDENCIES_COMMAND = "apt-cache rdepends libgcc-s1" # Ubuntu
-DOWNLOAD_COMMAND = "mkdir $dir && cd $dir && apt-get download $package"
-EXTRACT_DATA_COMMAND = (
-    "cd $dir && ar x *.deb data.tar.xz data.tar.gz && rm *.deb"
-)
-UNTAR_COMMAND = "tar xvf $dir/data.tar.* --directory $dir && rm $dir/data.tar.*"
-FIND_COMMAND = "test -d $dir/$subpath && find $dir/$subpath" # -size -2M"
-DIS_COVER_COMMAND = "dis-cover -p $filename"
+DOWNLOAD_COMMAND = "mkdir $dir && cd $dir && apt-get source $package"
+GREP_COMMAND = "grep -r -- -fno-rtti $dir"
 CLEANUP_COMMAND = "rm $dir -rf"
 
 
@@ -34,52 +30,33 @@ def run_command(command, shell=False):
 
 
 def analyze_package(package):
-    data = {}
+    data = ""
     dir_name = str(uuid.uuid4())
 
     try:
         download_command = DOWNLOAD_COMMAND.replace("$package", package).replace("$dir", dir_name)
         run_command(download_command, shell=True)
-
-        extract_command = EXTRACT_DATA_COMMAND.replace("$dir", dir_name)
-        run_command(extract_command, shell=True)
-
-        untar_command = UNTAR_COMMAND.replace("$dir", dir_name)
-        run_command(untar_command, shell=True)
     except:
         remove_command = CLEANUP_COMMAND.replace("$dir", dir_name)
         run_command(remove_command, shell=True)
-        return {"could_not_extract": True}
+        return "could_not_extract"
 
-    files = []
+    grep_command = GREP_COMMAND.replace("$dir", dir_name)
 
-    for directory in ["usr/bin", "usr/sbin", "usr/lib", "usr/games"]:
-        find_command = FIND_COMMAND \
-            .replace("$subpath", directory) \
-            .replace("$dir", dir_name)
-        try:
-            files += run_command(find_command, shell=True).split()
-        except RuntimeError:
-            pass
+    try:
+        data = run_command(grep_command, shell=True).split()
+    except:
+        pass
     
-    if len(files) == 0:
-        remove_command = CLEANUP_COMMAND.replace("$dir", dir_name)
-        run_command(remove_command, shell=True)
-        return {"no_file_found": True}
-
-    for filename in files:
-        dis_cover_command = DIS_COVER_COMMAND.replace("$filename", filename)
-        try:
-            data[filename] = pickle.loads(run_command(dis_cover_command))
-        except RuntimeError:
-            pass
-
     remove_command = CLEANUP_COMMAND.replace("$dir", dir_name)
     run_command(remove_command, shell=True)
     return data
 
 
 if __name__ == "__main__":
+    # We add the source link
+    run_command(ADD_SOURCE_LIST, shell=True)
+
     # We apt update.
     run_command(UPDATE_COMMAND)
 
